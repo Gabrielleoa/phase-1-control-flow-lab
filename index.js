0fdc6ac9878e0b6bfc493998c5438d18280ed88d
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-02 21:49:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if(someValue <= 400){
+    result = "This one is on me!";
+  }
+  if (someValue > 2000){
+    result = 'I will gladly take your thirty bucks.';
+  }
+  if (someValue > 2500){
+    result = 'No can do.';
+  }
+  return result
+} 
+function ternaryCheckCity(city){
+  let Destination; 
+  if (city === 'NYC'){
+     Destination= "Ok, sounds good.";
+  } else if (city != 'NYC'){
+    Destination = 'No go.';
+  }
+  return Destination
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.';
+    }
+  }
